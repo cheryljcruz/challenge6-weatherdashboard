58b7822e4fafa85b4ff2237db5ba9654f77bad74
koiclone <cheryljcruz1@gmail.com>
2022-03-19 21:30:16 -0500

    format second api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var cityForm = document.querySelector(".city-search-form");
 var cityInput = document.querySelector(".search-city");
+var currentCity = document.querySelector(".current-city-name");
 
 
 
@@ -10,9 +11,18 @@ var getWeather = function (cityName) {
     // make a get request to url
     fetch(apiUrl)
         .then(function (response) {
-            //request was successful
             response.json().then(function (data) {
                 console.log(data, cityName);
+                var lon = data[0].lon;
+                var lat = data[0].lat;
+                //display city / weather
+                var todayName = data[0].name;
+                currentCity.textContent = todayName;
+                console.log(data);
+
+                //format second api url
+                var apiUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=7570d2477f6e5e833116781b24b1cf5c&units=imperial";
+                
 
             });
 
@@ -35,6 +45,10 @@ var formSubmitHandler = function (event) {
         alert("Please enter a city");
     }
 };
- console.log(formSubmitHandler);
+
+
+
+
+
 // add event listeners to form and button container
 cityForm.addEventListener("submit", formSubmitHandler);
