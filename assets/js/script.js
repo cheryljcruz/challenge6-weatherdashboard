671b12c355ec4b7a7b704f86c179c6e74f082f43
koiclone <cheryljcruz1@gmail.com>
2022-03-20 22:13:59 -0500

    tiles display future forecast data: wind speed, humidity, temperature

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,23 @@ var currentCity = document.querySelector(".current-city-name");
 var currentIcon = document.querySelector(".current-icon");
 var currentDate = document.querySelector(".current-date");
 var currentTemp = document.querySelector(".current-temperature")
-var currentHumidity= document.querySelector(".current-humidity");
+var currentHumidity = document.querySelector(".current-humidity");
 var currentWind = document.querySelector(".current-wind-speed");
 var currentUV = document.querySelector(".current-uv-index");
 
+//five day forecast
+//day one
+var tempOne = document.querySelector(".one-temp");
+var windOne = document.querySelector(".one-wind");
+var humidOne = document.querySelector(".one-humid");
+//day two
+
+//daythree
+//dayfour
+//dayfive
 
 
+// both api calls 
 var getWeather = function (cityName) {
     //format the first api url
     var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=7570d2477f6e5e833116781b24b1cf5c&units=imperial";
@@ -37,6 +48,7 @@ var getWeather = function (cityName) {
                                 .then(function (data) {
                                     console.log(data);
                                     currentWeather(data);
+                                    fiveDayForecast(data);
 
                                 })
                         })
@@ -47,7 +59,7 @@ var getWeather = function (cityName) {
 
 
 };
-
+//search form
 var formSubmitHandler = function (event) {
     //prevent page refresh
     event.preventDefault();
@@ -62,10 +74,37 @@ var formSubmitHandler = function (event) {
         alert("Please enter a city");
     }
 };
-
+//display current weather
 var currentWeather = function (data) {
+    //current icon
+    var currentImg = data.current.weather[0].icon;
+    // create element for icon to diplay
+
+    //current temp display
     var currentTemperature = data.current.temp;
-    currentTemp.textContent= currentTemperature;
+    currentTemp.textContent = currentTemperature;
+    //current humidity display
+    var currentHumid = data.current.humidity;
+    currentHumidity.textContent = currentHumid;
+    //current windspeed
+    var currentWindSpeed = data.current.wind_speed;
+    currentWind.textContent = currentWindSpeed;
+    //current uv index
+    var currentUVIndex = data.current.uvi;
+    currentUV.textContent = currentUVIndex;
+
+};
+
+//five day forecast
+var fiveDayForecast = function (data) {
+    var dayonetemp = data.daily[0].temp.day;
+    tempOne.textContent = dayonetemp;
+    var dayonewind = data.daily[0].wind_speed;
+    windOne.textContent = dayonewind;
+    var dayonehumid = data.daily[0].humidity;
+    humidOne.textContent= dayonehumid;
+
+
 }
 
 
